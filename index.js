158521abe20284398eab04c32d03495d9066bbfe
benjaminPla <benjamin.pla@7-11.com>
2023-10-05 20:55:12 +0200

    add /login endpoint, add try-catch inside /callback endpoint

--- index.js
+++ index.js
@@ -5,25 +5,42 @@ import "dotenv/config";
 
 const api = express();
 
-api.get("/callback", async (req, res) => {
+api.get("/login", (req, res) => {
+  const url =
+    "https://login.microsoftonline.com/" +
+    `${process.env.TENANT_ID}` +
+    "/oauth2/v2.0/authorize" +
+    `?client_id=${process.env.CLIENT_ID}` +
+    `&response_type=${process.env.RESPONSE_TYPE}` +
+    `&scope=${process.env.SCOPE}`;
+  res.redirect(url);
+});
+
+api.get("/login/callback", async (req, res) => {
   const { code } = req?.query;
 
-  const request = await axios.post(
-    `https://login.microsoftonline.com/${process.env.TENANT_ID}/oauth2/v2.0/token`,
-    qs.stringify({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      code,
-      grant_type: process.env.GRANT_TYPE,
-      redirect_uri: process.env.REDIRECT_URI,
-    }),
-    {
-      "Content-Type": "application/x-www-form-urlencoded",
-    }
-  );
+  try {
+    const request = await axios.post(
+      `https://login.microsoftonline.com/${process.env.TENANT_ID}/oauth2/v2.0/token`,
+      qs.stringify({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code,
+        grant_type: process.env.GRANT_TYPE,
+        redirect_uri: process.env.REDIRECT_URI,
+      }),
+      {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+    );
 
-  console.log(request.data);
-  res.json(request.data);
+    console.log(request.data);
+    res.json(request.data);
+  } catch (error) {
+    const { status, data } = error.response;
+    console.error(data);
+    res.status(status).json(data);
+  }
 });
 
 const port = process.env.PORT || 3000;
